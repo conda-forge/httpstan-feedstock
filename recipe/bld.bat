2e258f900f400f1cb1a160ff2659ebab4fa096a3
Adrian Seyboldt <adrian.seyboldt@gmail.com>
2022-06-18 16:23:22 +0200

    Fix make on windows and add STAN_THREADS variable

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,6 +1,7 @@
 @ECHO ON
 
-make
+set STAN_THREADS=TRUE
+mingw32-make -j%CPU_COUNT%
 if errorlevel 1 exit 1
 
 python -m pip install . --no-deps
--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,8 @@
 set -ex
 
 export TBB_CXX_TYPE=${c_compiler}
+export STAN_THREADS=True
 
-make -j2
+make -j1
 
 ${PYTHON} -m pip install --no-deps .
