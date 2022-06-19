ddc6246c564932a806b004087dd149e043809652
Adrian Seyboldt <adrian.seyboldt@gmail.com>
2022-06-19 20:38:40 +0200

    Call makefile

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,19 +2,19 @@
 
 set -ex
 
-#export STAN_THREADS=True
-#export TBB_CXX_TYPE=${c_compiler}
-#export TBB_INTERFACE_NEW=true
-#export TBB_INC=${PREFIX}/include/
-#export TBB_LIB=${PREFIX}/lib/
-#export PRECOMPILED_HEADERS=false
+export STAN_THREADS=True
+export TBB_CXX_TYPE=${c_compiler}
+export TBB_INTERFACE_NEW=true
+export TBB_INC=${PREFIX}/include/
+export TBB_LIB=${PREFIX}/lib/
+export PRECOMPILED_HEADERS=false
 
 #echo "prefix is " ${PREFIX}
 
-#make conda -j1
+make conda -j1
 
-$PYTHON -m poetry build
-$PYTHON -m pip install dist/*.whl -vv
+${PYTHON} -m poetry build
+${PYTHON} -m pip install dist/*.whl -vv
 #$PYTHON -m pip install .
 #$PYTHON -m pip install .
 
