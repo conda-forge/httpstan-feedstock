81eaea3226632386f17d04a8c8453c099f963e0b
Adrian Seyboldt <adrian.seyboldt@gmail.com>
2022-06-20 15:37:42 +0200

    Add eigen3 to cxxflags on windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,8 +5,9 @@ set TBB_INTERFACE_NEW=true
 set TBB_INC=%PREFIX%/include/
 set TBB_LIB=%PREFIX%/lib/
 set PRECOMPILED_HEADERS=false
-set CXXFLAGS=-I%LIBRARY_INC%
+set CXXFLAGS=-I%LIBRARY_INC% -I%LIBRARY_INC%/eigen3
 
+dir %LIBRARY_INC%
 
 mingw32-make -f Makefile.conda conda -j1
 if errorlevel 1 exit 1
